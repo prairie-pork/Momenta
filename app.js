ad0483cff1a5cc69b95ae3b589e26cd1100c5dac
Hosea <hoseamfcfarms@gmail.com>
2026-06-26 23:17:22 -0500

    continued to improve design of calender

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ let currentYear = currentDate.getFullYear();
 //Grabbing all 42 day cells at once
 const dayCells = document.querySelectorAll(".day-cell")
 
-//CODE THAT I MAY NOT NEED ANYMORE BUT DON'T KNOW FOR SURE:
-//---------------------------------------------------------
-//const currentMonthNumber = currentDate.getMonth();
-//let currentMonthIndex = currentMonthNumber;
-//---------------------------------------------------------
-
 const months = [
     "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"
@@ -31,8 +25,8 @@ function updateCalendar() {
     //loops through all boxes and wipes them blank.
     for (let i = 0; i < dayCells.length; i++) {
         dayCells[i].textContent = "";
+        dayCells[i].style.display = "flex";
     }
-
 
     //calculates calendar metrics for this specific month
     const firstDayIndex = new Date(currentYear, currentMonthIndex, 1).getDay();
@@ -43,7 +37,15 @@ function updateCalendar() {
         const targetSlotIndex = firstDayIndex + day - 1;
         dayCells[targetSlotIndex].textContent = day;
     }
+
+    //checks if 6th row is empty and hides it if needed
+    if (dayCells[35].textContent === '') {
+        for (let i = 35; 1 < 42; i++) {
+            dayCells[i].style.display = "none";
+        }
+    }
 }
+
 prevBtn.addEventListener("click", () => {
     currentMonthIndex = currentMonthIndex - 1;
     if (currentMonthIndex < 0) {
